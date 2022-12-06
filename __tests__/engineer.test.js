6abd9720feff50e5eec5cf257e6af4dc4b08bdf8
JaredSchips <jared.schips@gmail.com>
2022-12-06 18:42:32 -0500

    Add engineer class test

---
+++ __tests__/engineer.test.js
@@ -0,0 +1,34 @@
+Employee = require('../lib/employee')
+Engineer = require('../lib/engineer')
+const { randomWord, randomInt } = require('../src/test-utils')
+
+randomEngineer = () => new Engineer(randomWord(), randomInt(), `${randomWord()}@email.com`, randomInt())
+
+describe('Engineer', () => {
+    describe('Initialization', () => {
+        it('should be an Employee', () => {
+            const someEngineer = randomEngineer()
+            expect(someEngineer).toBeInstanceOf(Employee)
+        });
+
+        it('should have property "github"', () => {
+            const someGithub = randomWord()
+            const someEngineer = new Engineer(randomWord(), randomInt(), `${randomWord()}@email.com`, someGithub)
+            expect(someEngineer.github).is(someGithub)
+        });
+    })
+
+    describe('getGithub', () => {
+        it('should return the github property', () => {
+            const someEngineer = randomEngineer()
+            expect(someEngineer.getGithub()).is(someEngineer.github)
+        });
+    })
+
+    describe('getRole', () => {
+        it('should return "Engineer"', () => {
+            const someEngineer = randomEngineer()
+            expect(someEngineer.getRole()).is('Engineer')
+        })
+    })
+})
