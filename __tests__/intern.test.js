8ba215e49708c2029805bcaa9d513eb5ed5c35f7
JaredSchips <jared.schips@gmail.com>
2022-12-06 19:55:37 -0500

    Add intern class test

---
+++ __tests__/intern.test.js
@@ -0,0 +1,34 @@
+Employee = require('../lib/employee')
+Intern = require('../lib/intern')
+const { randomWord, randomInt } = require('../src/test-utils')
+
+randomIntern = () => new Intern(randomWord(), randomInt(), `${randomWord()}@email.com`, randomWord())
+
+describe('Intern', () => {
+    describe('Initialization', () => {
+        it('should be an Employee', () => {
+            const someIntern = randomIntern()
+            expect(someIntern).toBeInstanceOf(Employee)
+        });
+
+        it('should have property "school"', () => {
+            const someSchool = randomWord()
+            const someIntern = new Intern(randomWord(), randomInt(), `${randomWord()}@email.com`, someSchool)
+            expect(someIntern.school).is(someSchool)
+        });
+    })
+
+    describe('getSchool', () => {
+        it('should return the school property', () => {
+            const someIntern = randomIntern()
+            expect(someIntern.getSchool()).is(someIntern.school)
+        });
+    })
+
+    describe('getRole', () => {
+        it('should return "Intern"', () => {
+            const someIntern = randomIntern()
+            expect(someIntern.getRole()).is('Intern')
+        })
+    })
+})
